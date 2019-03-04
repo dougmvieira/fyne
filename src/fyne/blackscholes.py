0debd5342dd921c4d6e81e53c905afa58ceb0d1a
Doug V <dougmvieira@gmail.com>
2019-03-04 00:16:09 +0000

    Improved Black-Scholes interface

--- src/fyne/blackscholes.py
+++ src/fyne/blackscholes.py
@@ -3,8 +3,39 @@ from scipy.optimize import newton
 from scipy.stats import norm
 
 
-def formula(k, t, sigma):
+def formula(underlying_price, strike, maturity, sigma):
     """Black-Scholes formula
+    """
+
+    k = np.log(strike/underlying_price)
+    return _reduced_formula(k, maturity, sigma)*underlying_price
+
+
+def vega(underlying_price, strike, maturity, sigma):
+    """Black-Scholes Greek vega
+    """
+
+    k = np.log(strike/underlying_price)
+    return _reduced_vega(k, maturity, sigma)*underlying_price
+
+
+def implied_vol(underlying_price, strike, maturity, option_price,
+                initial_guess=0.5):
+    """Implied volatility function
+    """
+    if (option_price <= underlying_price - strike
+            or option_price >= underlying_price):
+        raise ValueError("Option price outside no-arbitrage bounds")
+
+    k = np.log(strike/underlying_price)
+    c = option_price/underlying_price
+    return _reduced_implied_vol(k, maturity, c, initial_guess)
+
+
+def _reduced_formula(k, t, sigma):
+    """Reduced Black-Scholes formula
+
+    Used in `fyne.blackscholes.formula`.
     """
     tot_std = sigma*np.sqrt(t)
     d_plus = tot_std/2 - k/tot_std
@@ -13,8 +44,10 @@ def formula(k, t, sigma):
     return norm.cdf(d_plus) - norm.cdf(d_minus)*np.exp(k)
 
 
-def vega(k, t, sigma):
-    """Black-Scholes Greek vega
+def _reduced_vega(k, t, sigma):
+    """Reduced Black-Scholes Greek vega
+
+    Used in `fyne.blackscholes.vega`.
     """
     tot_std = sigma*np.sqrt(t)
     d_plus = tot_std/2 - k/tot_std
@@ -22,11 +55,11 @@ def vega(k, t, sigma):
     return norm.pdf(d_plus)*np.sqrt(t)
 
 
-def implied_vol(k, t, c, iv0=0.5):
-    """Implied volatility function
-    """
-    if c <= 0 or c >= 1:
-        raise ValueError("Option price outside no-arbitrage bounds")
+def _reduced_implied_vol(k, t, c, iv0):
+    """Reduced Implied volatility function
 
-    return newton(lambda iv: formula(k, t, iv) - c, iv0,
-                  lambda iv: vega(k, t, iv))
+    Used in `fyne.blackscholes.implied_vol`.
+    """
+
+    return newton(lambda iv: _reduced_formula(k, t, iv) - c, iv0,
+                  lambda iv: _reduced_vega(k, t, iv))
--- tests/test_fyne.py
+++ tests/test_fyne.py
@@ -1,9 +1,46 @@
+from pytest import raises
 from fyne import blackscholes
 
 
 def test_impliedvol():
-    vol, k, t = 0.2, 0.1, 0.5
-    c = blackscholes.formula(k, t, vol)
-    iv = blackscholes.implied_vol(k, t, c)
+    sigma = 0.2
+    underlying_price = 100.
+    strike = 90.
+    maturity = 0.5
 
-    assert abs(vol - iv) < 1e-6
+    option_price = blackscholes.formula(underlying_price, strike, maturity,
+                                        sigma)
+    iv = blackscholes.implied_vol(underlying_price, strike, maturity,
+                                  option_price)
+
+    assert abs(sigma - iv) < 1e-6
+
+
+def test_impliedvol_exception():
+    underlying_price = 100.
+    strike = 90.
+    maturity = 0.5
+
+    option_price = 9.
+    with raises(ValueError):
+        blackscholes.implied_vol(underlying_price, strike, maturity,
+                                 option_price)
+
+    option_price = 101.
+    with raises(ValueError):
+        blackscholes.implied_vol(underlying_price, strike, maturity,
+                                 option_price)
+
+
+def test_vega():
+    sigma = 0.2
+    underlying_price = 100.
+    strike = 90.
+    maturity = 0.5
+
+    vega_exact = blackscholes.vega(underlying_price, strike, maturity, sigma)
+    vega_finite_diffs = 500.*(
+        blackscholes.formula(underlying_price, strike, maturity, sigma + .001)
+        - blackscholes.formula(underlying_price, strike, maturity, sigma - .001))
+
+    assert abs(vega_exact - vega_finite_diffs) < 1e-3
