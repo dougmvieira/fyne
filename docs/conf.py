e0b8eaa1a7bf176ab618655196f8e4a1dcbefa58
Doug V <dougmvieira@gmail.com>
2019-03-23 21:23:36 +0000

    Bump version: 0.3.0 → 0.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ project = 'fyne'
 year = '2019'
 author = 'Douglas Machado Vieira'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.3.0'
+version = release = '0.4.0'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- src/fyne/__init__.py
+++ src/fyne/__init__.py
@@ -7,4 +7,4 @@ Please see below the documentation for the implemented affine models.
 
 """
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
